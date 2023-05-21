30fbdb4d41ffbdf511675c204b87ab5722ae9df0
Eduardo Marques <edu.temarques@gmail.com>
2023-05-21 10:35:24 -0300

    wip: integrated person to home page

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,12 +1,17 @@
 import { Component, OnDestroy, OnInit } from '@angular/core';
-import { ConfigurationsService } from './../shared/services/configurations.service';
-
+import { PageEvent } from '@angular/material/paginator';
 import { Subject, takeUntil, tap } from 'rxjs';
-import { INavbarParameters } from '../shared/components/navbar/model/navbar-parameters.models';
-import { ISearchFilter } from '../shared/components/search-input/model/search-filter.model';
-import { ISearchInputParameters } from '../shared/components/search-input/model/search-input-parameters.model';
-import { IMenuActionButton } from '../shared/model/menu-action-button.model';
-import { ISystemValue } from '../shared/model/system-value.model';
+
+import { IPerson } from './../person/models/person.model';
+import { PersonService } from './../person/services/person.service';
+import { INavbarParameters } from './../shared/components/navbar/model/navbar-parameters.models';
+import { ISearchFilter } from './../shared/components/search-input/model/search-filter.model';
+import { ISearchInputParameters } from './../shared/components/search-input/model/search-input-parameters.model';
+import { IMenuActionButton } from './../shared/model/menu-action-button.model';
+import { IPaginatedResponse } from './../shared/model/paginated-response.model';
+import { IPaginationParameters } from './../shared/model/pagination-parameters.model';
+import { ISystemValue } from './../shared/model/system-value.model';
+import { ConfigurationsService } from './../shared/services/configurations.service';
 import { homeNavbarConfiguration } from './configurations/home-navbar.configuration';
 import { homeSearchConfiguration } from './configurations/home-search.configuration';
 
@@ -18,20 +23,29 @@ import { homeSearchConfiguration } from './configurations/home-search.configurat
 export class HomeComponent implements OnInit, OnDestroy {
   public homeNavbarParameters: INavbarParameters = homeNavbarConfiguration;
   public homeSearchParameters: ISearchInputParameters = homeSearchConfiguration;
+  public paginationParameters: IPaginationParameters = {};
+  public personList: IPerson[] = [];
 
   private onDestroy$: Subject<void> = new Subject<void>();
 
-  constructor(private configurationService: ConfigurationsService) {
+  constructor(private configurationService: ConfigurationsService, private personService: PersonService) {
     this.configureLogoutAction();
+    this.configurePagination();
   }
 
   ngOnInit(): void {
     this.listAllSearchOptions();
+    this.listAllPersons();
   }
 
   ngOnDestroy(): void {
     this.onDestroy$.next();
     this.onDestroy$.unsubscribe();
+  }
+
+  public paginationEvent(page: PageEvent): void {
+    this.configurePagination(page.pageIndex, page.pageSize);
+    this.listAllPersons();
   }
 
   public searchEvent(searchFilter: ISearchFilter) {
@@ -46,9 +60,34 @@ export class HomeComponent implements OnInit, OnDestroy {
     });
   }
 
+  private configurePagination(pageIndex?: number, pageSize?: number, pageSizeOptions?: number[], totalCount?: number): void {
+    this.paginationParameters.pageIndex = pageIndex || 0;
+    this.paginationParameters.pageSize = pageSize || 10;
+    this.paginationParameters.pageSizeOptions = pageSizeOptions || [1, 10, 50, 100];
+    this.paginationParameters.totalCount = totalCount || 0;
+  }
+
+  private listAllPersons(): void {
+    console.log('Teste: ', this.paginationParameters);
+    this.personService
+      .listAll(this.paginationParameters)
+      .pipe(
+        tap((response: IPaginatedResponse<IPerson>) => {
+          this.personList = response.content || [];
+          this.paginationParameters = {
+            ...this.paginationParameters,
+            pageIndex: response.pageNumber,
+            pageSize: response.pageSize,
+            totalCount: response.totalElements,
+          };
+        })
+      )
+      .subscribe();
+  }
+
   private listAllSearchOptions(): void {
-    this.configurationService.listAllSearchOptions();
-    this.configurationService.searchOptions$
+    this.configurationService
+      .listAllSearchOptions()
       .pipe(
         tap((response: ISystemValue[]) => (this.homeSearchParameters.searchTypeList = response)),
         takeUntil(this.onDestroy$)
--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -2,6 +2,7 @@ import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { MatCardModule } from '@angular/material/card';
 
+import { PersonModule } from '../person/person.module';
 import { NavbarModule } from '../shared/components/navbar/navbar.module';
 import { SearchInputModule } from '../shared/components/search-input/search-input.module';
 import { HomeRoutingModule } from './home-routing.module';
@@ -9,6 +10,6 @@ import { HomeComponent } from './home.component';
 
 @NgModule({
   declarations: [HomeComponent],
-  imports: [CommonModule, HomeRoutingModule, MatCardModule, NavbarModule, SearchInputModule],
+  imports: [CommonModule, HomeRoutingModule, MatCardModule, NavbarModule, PersonModule, SearchInputModule],
 })
 export class HomeModule {}
